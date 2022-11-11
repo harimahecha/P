2caaef8b032157b4bc5ebaa9951776045e51668c
harimahecha <harimahechawc@gmail.com>
2022-11-11 15:56:48 +0530

    ALL

---
+++ JS/auto.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".home_text_auto", {
+  strings: ["", "Web Developement", "Programming"],
+  typeSpeed: 75,
+  backSpeed: 50,
+  loop: true,
+});
---
+++ JS/menu.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById("menu");
+
+function openmenu(){
+    menu.style.right = "0";
+}
+
+function closemenu(){
+    menu.style.right = "-200px";
+}
